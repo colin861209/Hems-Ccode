185f08a4021c5e1b52635366cc8ff5bbebc2da3b
colinwang <colinwang@kkstream.com>
2020-03-14 20:34:44 +0800

    change newest main cpp

--- HEMS_GLPK/main.cpp
+++ HEMS_GLPK/main.cpp
@@ -18,10 +18,10 @@ double z = 0;
 int time_block = 0, app_count = 0, variable = 0, divide = 0, sample_time = 0, sa_counter = 0, rasa_counter; float delta_T = 0.0;
 float Cbat = 0.0, Vsys = 0.0, SOC_ini = 0.0, SOC_min = 0.0, SOC_max = 0.0, SOC_thres = 0.0, Pbat_min = 0.0, Pbat_max = 0.0, Pgrid_max = 0.0, Psell_max, Delta_battery = 0.0, Pfc_max = 0.0;
 float step1_bill = 0.0, step1_sell = 0.0, step1_PESS = 0.0;	
-
+// app parameter		
 // app parameter		
 int interrupt_num = 0, uninterrupt_num = 0, varying_num = 0, ponit_num = 0;
-// power
+int stop_enable = 0;
 int stop_enable = 0;
 char column[400] = "A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19,A20,A21,A22,A23,A24,A25,A26,A27,A28,A29,A30,A31,A32,A33,A34,A35,A36,A37,A38,A39,A40,A41,A42,A43,A44,A45,A46,A47,A48,A49,A50,A51,A52,A53,A54,A55,A56,A57,A58,A59,A60,A61,A62,A63,A64,A65,A66,A67,A68,A69,A70,A71,A72,A73,A74,A75,A76,A77,A78,A79,A80,A81,A82,A83,A84,A85,A86,A87,A88,A89,A90,A91,A92,A93,A94,A95";
 
@@ -42,13 +42,11 @@ int main(void)
 	RT_enable = 0;
 
 	//MARK: one_day scheduling MYSQL read PARM
-	MYSQL *mysql_con = mysql_init(NULL);
 	MYSQL_RES *mysql_result;
 	MYSQL_ROW mysql_row;
 
 	int same_day = 0, real_time = 0;
-
-	float *base_par = new float[16];
+	int same_day = 0, real_time = 0;
 	char *s_time = new char[3];
 	int *position = new int[16];
 
@@ -69,7 +67,20 @@ int main(void)
 
 	//if just want only run realtime 24 hour.. otherwise you can commend it
 	snprintf(sql_buffer, sizeof(sql_buffer), "SELECT value FROM LP_BASE_PARM WHERE parameter_id = 27");//get now_SOC
+	//if just want only run realtime 24 hour.. otherwise you can commend it
+	snprintf(sql_buffer, sizeof(sql_buffer), "SELECT value FROM LP_BASE_PARM WHERE parameter_id = 27");//get now_SOC
 	mysql_query(mysql_con, sql_buffer);
+	mysql_result = mysql_store_result(mysql_con);
+	mysql_row = mysql_fetch_row(mysql_result);
+	stop_enable =atoi(mysql_row[0]);
+	if (stop_enable == 1)
+	{
+		printf("A day is over so force to close program....\n");
+		return -1;
+	}
+
+
+	//SELECT COLUMN_NAME FROM `COLUMNS` WHERE `COLUMN_NAME` BETWEEN 'A0' AND 'A95'
 	mysql_result = mysql_store_result(mysql_con);
 	mysql_row = mysql_fetch_row(mysql_result);
 	stop_enable =atoi(mysql_row[0]);
@@ -96,8 +107,6 @@ int main(void)
 	mysql_result = mysql_store_result(mysql_con);
 	mysql_row = mysql_fetch_row(mysql_result);
 	uninterrupt_num = atoi(mysql_row[0]);
-	mysql_free_result(mysql_result);
-	printf("uninterruptable app num:%d\n", uninterrupt_num);
 
 	snprintf(sql_buffer, sizeof(sql_buffer), "SELECT count(*) AS numcols FROM load_list WHERE group_id=3 "); // 變動型 uninterrupt_num = 1
 	mysql_query(mysql_con, sql_buffer);
@@ -108,24 +117,25 @@ int main(void)
 	printf("variable app num:%d\n", varying_num);
 
 	// MARK: s_time => last time execute 2020-0x-0x => time_tmp[3] = {2020, 03, 06} 
-	//		to check whether same day or not, 
+		{ base_par[i - 1] = atof(mysql_row[0]);	}
 	//  	if different,then enforce start up one_schedule => same day = 0 (一日排程)
 	for (i = 1; i <= 17; i++)
 	{
 		snprintf(sql_buffer, sizeof(sql_buffer), "select value from LP_BASE_PARM where parameter_id = '%d'", i);
 		mysql_query(mysql_con, sql_buffer);
-		mysql_result = mysql_store_result(mysql_con);
-		mysql_row = mysql_fetch_row(mysql_result);
-		if (i < 17)
-		{ base_par[i - 1] = atof(mysql_row[0]);	}
-		else
-		{ s_time = mysql_row[0]; }
-		mysql_free_result(mysql_result);
-	}
-
 	//check whether same day or not(if different,then enforce start up one_schedule)
-	//VS2015
+		mysql_result = mysql_store_result(mysql_con);
 	 // char *p;
+	 // char *token = strtok_s(s_time, "-", &p);
+	 // int time_tmp[3], tt = 0;
+	 // while (token != NULL)
+	 // {
+	 // 	time_tmp[tt] = atoi(token);
+	 // 	token = strtok_s(NULL, "-", &p);
+	 // 	tt++;
+	 // }
+	 // char *p;
+	//linux
 	 // char *token = strtok_s(s_time, "-", &p);
 	 // int time_tmp[3], tt = 0;
 	 // while (token != NULL)
@@ -141,32 +151,30 @@ int main(void)
 	while (token != NULL)
 	{
 		time_tmp[tt] = atoi(token);
-		token = strtok(NULL, "-");
+	{same_day = 1;}	
 		tt++;
-	}
-
-	if ((time_tmp[0] != (now_time.tm_year + 1900)) || (time_tmp[1] != (now_time.tm_mon + 1)) || (time_tmp[2] != (now_time.tm_mday)))
-	{
-		same_day = 0;
+	//send parameter to variale number
+	time_block = base_par[0]; // = 96 
+	app_count = interrupt_num + uninterrupt_num + varying_num;	// = 12+2+1 = 15
+	ponit_num = 6;
+	variable = app_count + 9 + uninterrupt_num + (varying_num * 2) + 2 + 2 * (ponit_num - 1) + 3;	// 15+9+2+(1*2)+2+2*(6-1)+3 = 43
 		real_time = 0;
 	}
-	else
+	Vsys = base_par[5]; // = 48
 	{same_day = 1;}	
 
 	//send parameter to variale number
 	time_block = base_par[0]; // = 96 
 	app_count = interrupt_num + uninterrupt_num + varying_num;	// = 12+2+1 = 15
 	ponit_num = 6;
-	variable = app_count + 9 + uninterrupt_num + (varying_num * 2) + 2 + 2 * (ponit_num - 1) + 3;	// 15+9+2+(1*2)+2+2*(6-1)+3 = 43
-	divide = (time_block / 24); // = 4
-	delta_T = 1.0 / (float)divide; // = 0.25
-	Vsys = base_par[5]; // = 48
+	Pbat_max = 1.53;
+	Pbat_min = 1.53;
+	//Pfc_max = 5.13;
+	Pfc_max = 0.0;
 	Cbat = base_par[6];	// = 0.318
-	SOC_min = base_par[7];	// = 0.6
-	SOC_max = base_par[8];	// = 0.9
-	SOC_thres = base_par[9];	// = 0.7
-	//Pbat_max = 0.1*Vsys*Cbat;
-	//Pbat_min = 0.1*Vsys*Cbat;
+	Pgrid_max = base_par[12];	
+	Psell_max = base_par[13];	
+	real_time = (int)base_par[14];	
 	Pbat_max = 1.53;
 	Pbat_min = 1.53;
 	//Pfc_max = 5.13;
@@ -175,13 +183,24 @@ int main(void)
 	Pgrid_max = base_par[12];	
 	Psell_max = base_par[13];	
 	real_time = (int)base_par[14];	
+	/*=============================get sample time=============================*/
+	 
+	if (((now_time.tm_min) % (60 / divide)) != 0)
+	{
+		sample_time = (now_time.tm_hour) * divide + (int)((now_time.tm_min) / (60 / divide)) ;  
+	}
+	else
+	{
+		sample_time = (now_time.tm_hour) * divide + (int)((now_time.tm_min) / (60 / divide)) ;
+	}
+	printf("sample_time:%d\n", sample_time);
+
 
 	base_par[1] = app_count;
 	base_par[2] = interrupt_num;
 	base_par[3] = uninterrupt_num;
 	base_par[4] = varying_num;
-	base_par[10] = Pbat_max;
-	base_par[11] = Pbat_min;
+	}
 
 	/*=============================get sample time=============================*/
 	 
@@ -213,7 +232,7 @@ int main(void)
 	printf("SOC min:%.2f\n", SOC_min);
 	printf("SOC max:%.2f\n", SOC_max);
 	printf("SOC threads:%.2f\n", SOC_thres);
-	printf("Pbat min:%.3f\n", Pbat_min);
+		snprintf(sql_buffer, sizeof(sql_buffer), "select number from load_list WHERE group_id<>0 ORDER BY group_id ASC,number ASC LIMIT %d,1", i);
 	printf("Pbat max:%.3f\n", Pbat_max);
 	printf("Pgrid max:%.2f\n", Pgrid_max);
 	printf("Psell max:%.2f\n", Psell_max);
@@ -226,7 +245,7 @@ int main(void)
 	// ------------------------------- Start to Put Mysql data into Matrix ------------------------------- //
 	float *price = new float[24];
 	float **INT_power = NEW2D(interrupt_num, 4, float);	// INT_power[interrupt_num][4]
-	float **UNINT_power = NEW2D(uninterrupt_num, 4, float);	// UNINT_power[uninterrupt_num][4]
+		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT start_time, end_time,operation_time ,power1 FROM load_list WHERE group_id = 1 ORDER BY number ASC LIMIT %d,1", i - 1);
 	float **VAR_power = NEW2D(varying_num, 9, float);	// VAR_power[varying_num][4]
 
 	// MARK: get all application(interrupt-> uninterrupt-> varying) into array position
@@ -240,9 +259,9 @@ int main(void)
 		//printf("xxxxxxx:%d\n", position[i]);
 		mysql_free_result(mysql_result);
 	}
-
+	for (i = 1; i < uninterrupt_num + 1; i++)
 	// MARK: get each interrupt application column value into multidimesional array INT_power
-	// 		Unit: start_time, end_time, operation_time (hour), power1, power2, power3 (KW), block1, block2, block3 (timeblock)
+		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT start_time, end_time,operation_time ,power1 FROM load_list WHERE group_id = 2 ORDER BY number ASC LIMIT %d,1", i - 1);
 	for (i = 1; i < interrupt_num + 1; i++)
 	{
 		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT start_time, end_time,operation_time ,power1 FROM load_list WHERE group_id = 1 ORDER BY number ASC LIMIT %d,1", i - 1);
@@ -256,9 +275,10 @@ int main(void)
 		}
 		//printf("\n");
 		mysql_free_result(mysql_result);
-	}
+	for (i = 1; i < varying_num + 1; i++)   //�ܰʫ�
 
-	// MARK: get each uninterrupt application column value into multidimesional array UNINT_power
+
+		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT start_time, end_time,operation_time,power1,power2,power3,block1,block2,block3 FROM load_list WHERE group_id = 3 ORDER BY number ASC LIMIT %d,1", i - 1);
 	for (i = 1; i < uninterrupt_num + 1; i++)
 	{
 		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT start_time, end_time,operation_time ,power1 FROM load_list WHERE group_id = 2 ORDER BY number ASC LIMIT %d,1", i - 1);
@@ -274,14 +294,15 @@ int main(void)
 		mysql_free_result(mysql_result);
 	}
 
-	// MARK: get each varying application column value into multidimesional array VAR_power
+		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT price_value FROM price WHERE price_period = %d", i - 1);
 	for (i = 1; i < varying_num + 1; i++)   //�ܰʫ�
 	{
 
-		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT start_time, end_time,operation_time,power1,power2,power3,block1,block2,block3 FROM load_list WHERE group_id = 3 ORDER BY number ASC LIMIT %d,1", i - 1);
+		price[i - 1] = atof(mysql_row[0]);
 		mysql_query(mysql_con, sql_buffer);
 		mysql_result = mysql_store_result(mysql_con);
 		mysql_row = mysql_fetch_row(mysql_result);
+
 		for (j = 0; j < 9; j++)
 		{
 			VAR_power[i - 1][j] = atof(mysql_row[j]);
@@ -299,7 +320,7 @@ int main(void)
 		mysql_result = mysql_store_result(mysql_con);
 		mysql_row = mysql_fetch_row(mysql_result);
 		price[i - 1] = atof(mysql_row[0]);
-		memset(sql_buffer, 0, sizeof(sql_buffer));
+	int *uninterrupt_flag = new int[uninterrupt_num];			
 		mysql_free_result(mysql_result);
 	}
 
@@ -343,12 +364,12 @@ int main(void)
 	}
 	for (j = 0; j < uninterrupt_num; j++)
 	{
-		uninterrupt_start[j] = 0;
+
 		uninterrupt_end[j] = 0;
 		uninterrupt_ot[j] = 0;
 		uninterrupt_reot[j] = 0;
-		uninterrupt_p[j] = 0.0;
-		uninterrupt_flag[j] = 0;
+		interrupt_start[i] = ((int)(INT_power[i][0] * divide));	
+		interrupt_end[i] = ((int)(INT_power[i][1] * divide)) - 1;
 	}
 	for (k = 0; k < varying_num; k++)
 	{
@@ -357,16 +378,17 @@ int main(void)
 		varying_ot[k] = 0;
 		varying_reot[k] = 0;
 		varying_flag[k] = 0;
-		for (j = 0; j < 3; j++)
+		uninterrupt_end[i] = ((int)(UNINT_power[i][1] * divide)) - 1;
 		{
 			varying_t_pow[k][j] = 0;
 			varying_p_pow[k][j] = 0.0;
+
 		}
 	}
 
 
 	// All the time has been converted to 96blocks, so multiply "divide". 
-	for (i = 0; i < interrupt_num; i++)
+		varying_start[i] = ((int)(VAR_power[i][0] * divide));	// varying_start[0] = 9
 	{
 		interrupt_start[i] = ((int)(INT_power[i][0] * divide));	
 		interrupt_end[i] = ((int)(INT_power[i][1] * divide)) - 1;
@@ -378,45 +400,23 @@ int main(void)
 	for (i = 0; i < uninterrupt_num; i++)
 	{
 		uninterrupt_start[i] = ((int)(UNINT_power[i][0] * divide));
-		uninterrupt_end[i] = ((int)(UNINT_power[i][1] * divide)) - 1;
 		uninterrupt_ot[i] = ((int)(UNINT_power[i][2] * divide));
 		uninterrupt_p[i] = UNINT_power[i][3];
 		printf("%d  %d   %d  %.3f  ", uninterrupt_start[i], uninterrupt_end[i], uninterrupt_ot[i], uninterrupt_p[i]);
 
 		printf("\n");
 	}
-
+	// ------------------------------- End of Put Mysql data into Matrix ------------------------------- //
 	for (i = 0; i < varying_num; i++)
-	{
-		varying_start[i] = ((int)(VAR_power[i][0] * divide));	// varying_start[0] = 9
-		varying_end[i] = ((int)(VAR_power[i][1] * divide))-1;	// varying_end[0] = 14
+	//clean table (if at one_schedule)
+	// MARK: if in the same day, only clean the 'real_status' table and get now_SOC
+	if (same_day == 1 && real_time == 1)
 		varying_ot[i] = ((int)(VAR_power[i][2] * divide));	// varying_ot[0] = 1
 		printf("%d  %d   %d  ", varying_start[i], varying_end[i], varying_ot[i]);
 		for (j = 0; j < 3; j++)
 		{
-			varying_p_pow[i][j] = VAR_power[i][3 + j];	// varying_p_pow[0][0~3] = [0.4, 0.5, 0.6] (kW)
-			printf("%.3f ", varying_p_pow[i][j]);
-		}
-		for (j = 0; j < 3; j++)
-		{
-			varying_t_pow[i][j] = ((int)(VAR_power[i][6 + j] * divide));	// varying_t_pow[0][0~3] = [1, 2, 1] (timeblock)
-			printf("%d ", varying_t_pow[i][j]);
-		}
-		printf("\n");
-	}
-
-	delete[] INT_power, UNINT_power, VAR_power;
-	// ------------------------------- End of Put Mysql data into Matrix ------------------------------- //
-
-	//clean table (if at one_schedule)
-	// MARK: if in the same day, only clean the 'real_status' table and get now_SOC
-	if (same_day == 1 && real_time == 1)
-	{
-		printf(" *********real_time mode**********\n");
-
-		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE real_status");      //clean real_status;
 		snprintf(sql_buffer, sizeof(sql_buffer), "SELECT value FROM LP_BASE_PARM WHERE parameter_id = 25");	//get now_SOC
-		mysql_query(mysql_con, sql_buffer);
+			varying_p_pow[i][j] = VAR_power[i][3 + j];	// varying_p_pow[0][0~3] = [0.4, 0.5, 0.6] (kW)
 		mysql_result = mysql_store_result(mysql_con);
 		mysql_row = mysql_fetch_row(mysql_result);
 		if ((atof(mysql_row[0]) / 100.0) < 1.00)
@@ -427,19 +427,19 @@ int main(void)
 		printf("NOW REAL SOC:%f", SOC_ini);
 		memset(sql_buffer, 0, sizeof(sql_buffer));
 		mysql_free_result(mysql_result);
-	}
+		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE real_status");      //clean real_status;
 	
-	// MARK: in each one day first time run this code, we clean the four table below, then update the real_time value to 1
-	else
-	{
-		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE control_status");      //clean control_status;
 		mysql_query(mysql_con, sql_buffer);
+		mysql_result = mysql_store_result(mysql_con);
+		mysql_row = mysql_fetch_row(mysql_result);
+		{SOC_ini = atof(mysql_row[0]);}
+		if (SOC_ini>90)
 		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE real_status");      //clean control_status;
 		mysql_query(mysql_con, sql_buffer);
 		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE cost");      //clean control_status;
 		mysql_query(mysql_con, sql_buffer);
 		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE solar_fake");      //clean control_status;
-		mysql_query(mysql_con, sql_buffer);
+		// SOC_ini=0.68;   //for test
 		//snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE solar_day");      //clean control_status;
 		//mysql_query(mysql_con, sql_buffer);
 
@@ -466,6 +466,53 @@ int main(void)
 			if ((atof(mysql_row[0]) / 100.0) < 1.00)
 			{SOC_ini = atof(mysql_row[0]);}
 			printf("ini_SOC:%f", SOC_ini);
+			memset(sql_buffer, 0, sizeof(sql_buffer));
+			mysql_free_result(mysql_result);
+		}
+
+	
+		real_time = 1;    //if you don't want do real_time,please commend it.
+	{
+		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE control_status");      //clean control_status;
+		mysql_query(mysql_con, sql_buffer);
+
+		mysql_query(mysql_con, sql_buffer);
+	//done and update_date
+		mysql_query(mysql_con, sql_buffer);
+		snprintf(sql_buffer, sizeof(sql_buffer), "TRUNCATE TABLE solar_fake");      //clean control_status;
+		mysql_query(mysql_con, sql_buffer);
+	//optimization
+	GLPK(interrupt_start, interrupt_end, interrupt_ot, interrupt_reot, interrupt_p, uninterrupt_start, uninterrupt_end, uninterrupt_ot, uninterrupt_reot, uninterrupt_p, uninterrupt_flag, varying_start, varying_end, varying_ot, varying_reot, varying_flag, varying_t_pow, varying_p_pow, app_count, price, position);
+			memset(sql_buffer, 0, sizeof(sql_buffer));
+			mysql_free_result(mysql_result);
+		}
+		else
+		{
+			printf(" *********one_day mode(caculate next day)**********\n");
+			snprintf(sql_buffer, sizeof(sql_buffer), "SELECT value FROM LP_BASE_PARM WHERE parameter_id = 25");//get now_SOC
+			mysql_query(mysql_con, sql_buffer);
+			mysql_result = mysql_store_result(mysql_con);
+			mysql_row = mysql_fetch_row(mysql_result);
+			if ((atof(mysql_row[0]) / 100.0) < 1.00)
+			{SOC_ini = atof(mysql_row[0]);}
+			printf("ini_SOC:%f", SOC_ini);
+	snprintf(sql_buffer, sizeof(sql_buffer), "UPDATE control_status SET parameter_id = '%d' WHERE  control_id = '%d'", 6, app_count + 11);
+	mysql_query(mysql_con, sql_buffer);
+
+
+	if ((sample_time+1) != 96)
+	{
+		same_day = 1;
+	}
+	else  
+	{
+		same_day = 0;
+
+		//if just want only run realtime 24 hour.. otherwise you can commend it
+		snprintf(sql_buffer, sizeof(sql_buffer), "UPDATE LP_BASE_PARM SET value = '%d' WHERE parameter_id = 27 ",1);
+		mysql_query(mysql_con, sql_buffer);
+	}
+
 			memset(sql_buffer, 0, sizeof(sql_buffer));
 			mysql_free_result(mysql_result);
 		}
@@ -520,5 +567,3 @@ int main(void)
 	system("pause");
 	return 0;
 }
-
-
