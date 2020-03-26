fcecd72fc0ceca266b2fae65da72757678057067
colinwang <colinwang@kkstream.com>
2020-03-26 21:09:41 +0800

    update

--- homework/interrupt vs grid/interrupt_vs_grid.cpp
+++ homework/interrupt vs grid/interrupt_vs_grid.cpp
@@ -8,8 +8,10 @@
 #include <iostream>
 #include "HEMS.h" 
 
-int interrupt_num = 0, int app_count = 0
-
+int interrupt_num = 0, app_count = 0, sample_time = 0, variable = 0;
+float Pgrid_max = 0.0;
+char sql_buffer[2000] = { 0 };
+char column[400] = "A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19,A20,A21,A22,A23,A24,A25,A26,A27,A28,A29,A30,A31,A32,A33,A34,A35,A36,A37,A38,A39,A40,A41,A42,A43,A44,A45,A46,A47,A48,A49,A50,A51,A52,A53,A54,A55,A56,A57,A58,A59,A60,A61,A62,A63,A64,A65,A66,A67,A68,A69,A70,A71,A72,A73,A74,A75,A76,A77,A78,A79,A80,A81,A82,A83,A84,A85,A86,A87,A88,A89,A90,A91,A92,A93,A94,A95";
 int main(void) {
     
     MYSQL *mysql_con = mysql_init(NULL);
@@ -25,15 +27,27 @@ int main(void) {
 	printf("Connect to Mysql sucess!!\n");
 	mysql_set_character_set(mysql_con, "utf8");
 
-    // get count of interrupt group 
-    snprintf(sql_buffer, sizeof(sql_buffer), "SELECT count(*) AS numcols FROM load_list WHERE group_id=1 "); 
+    // get count = 3 of interrupt group 
+    snprintf(sql_buffer, sizeof(sql_buffer), "SELECT count(*) AS numcols FROM load_list WHERE group_id=1 && number<6 "); 
 	mysql_query(mysql_con, sql_buffer);
 	mysql_result = mysql_store_result(mysql_con);
 	mysql_row = mysql_fetch_row(mysql_result);
-	interrupt_num = atoi(mysql_row[0]);
+	interrupt_num = atoi(mysql_row[0]); // 3
 	mysql_free_result(mysql_result);
+
+	snprintf(sql_buffer, sizeof(sql_buffer), "SELECT value FROM `LP_BASE_PARM` WHERE parameter_id = %d", 13);
+	mysql_query(mysql_con, sql_buffer);
+	mysql_result = mysql_store_result(mysql_con);
+	mysql_row = mysql_fetch_row(mysql_result);
+	Pgrid_max = atof(mysql_row[0]);	
+	mysql_free_result(mysql_result);
+
 	printf("interruptable app num:%d\n", interrupt_num);
-    app_count = interrupt_num
+	printf("Pgrid_max:%.2f\n", Pgrid_max);
+
+    app_count = interrupt_num;  // 3
+	variable = app_count + 1;  // 買電狀態
+	int *position = new int[app_count];
     float **INT_power = NEW2D(interrupt_num, 4, float);
 
     for (i = 1; i < interrupt_num + 1; i++) {
@@ -71,6 +85,7 @@ int main(void) {
 		interrupt_end[i] = ((int)(INT_power[i][1] * divide)) - 1;
 		interrupt_ot[i] = ((int)(INT_power[i][2] * divide));
 		interrupt_p[i] = INT_power[i][3];
+		printf("interrupt multi array: \n");
 		printf("%d  %d   %d  %.3f  ", interrupt_start[i], interrupt_end[i], interrupt_ot[i], interrupt_p[i]);
 		printf("\n");
 	
@@ -88,6 +103,149 @@ int main(void) {
 
 	}
 
-    // GLPK(interrupt_start, interrupt_end, interrupt_ot, interrupt_reot, interrupt_p, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, app_count, price, position);
+	for (i = 0; i < app_count; i++) {
+		snprintf(sql_buffer, sizeof(sql_buffer), "select number from load_list WHERE group_id<>0 ORDER BY group_id ASC,number ASC LIMIT %d,1", i);
+		mysql_query(mysql_con, sql_buffer);
+		mysql_result = mysql_store_result(mysql_con);
+		mysql_row = mysql_fetch_row(mysql_result);
+		position[i] = atoi(mysql_row[j]);
+		mysql_free_result(mysql_result);
+	}
+
+    // GLPK(interrupt_start, interrupt_end, interrupt_ot, interrupt_reot, interrupt_p, app_count, price, position);
+}
+
+void GLPK(int *interrupt_start, int *interrupt_end, int *interrupt_ot, int *interrupt_reot, float *interrupt_p, int app_count, float *price, int *position)
+{
+	int *buff = new int[app_count];	//存放剩餘執行次數(The number of remaining executions)
+	for (i = 0; i < app_count; i++)
+	{
+		buff[i] = 0;
+	}
+
+	int noo;
+	//get now time that can used in the real experiment
+	if (((now_time.tm_min) % (60 / divide)) != 0)
+	{
+		noo = (now_time.tm_hour) * divide + (int)((now_time.tm_min) / (60 / divide)) + 1;
+	}
+	else
+	{
+		noo = (now_time.tm_hour) * divide + (int)((now_time.tm_min) / (60 / divide));
+	}
+	printf("sample:%d", noo);
+
+	float *price2 = new float[time_block];
+	for (int x = 0; x < 24; x++)	
+	{
+		for (int y = x*divide; y < (x*divide)+divide; y++)
+		{
+			price2[y] = price[x];
+		}
+	}
+
+	for (i = 0; i < interrupt_num; i++)	//可中斷負載 (Interrupt load)
+	{
+		if ((interrupt_ot[i] - buff[i]) == interrupt_ot[i])
+		{
+			interrupt_reot[i] = interrupt_ot[i];
+		}
+		else if (((interrupt_ot[i] - buff[i]) < interrupt_ot[i]) && ((interrupt_ot[i] - buff[i]) > 0))
+		{
+			interrupt_reot[i] = interrupt_ot[i] - buff[i];
+		}
+		else if ((interrupt_ot[i] - buff[i]) <= 0)
+		{
+			interrupt_reot[i] = 0;
+		}
+	}
+
+	/*============================ 總規劃功率矩陣(Total planning power matrix) ====================================*/
+	float **power1 = NEW2D((((time_block - sample_time) * 1) + app_count), (variable * (time_block - sample_time)), float);
+
+	/*============================ GLPK參數矩陣定義(GLPK parameter matrix definition) ==================================*/
+	glp_prob *mip;
+	int *ia = new int[((((time_block - sample_time) * 1) + app_count) * (variable * (time_block - sample_time))) + 1]; 			// Row
+	int *ja = new int[((((time_block - sample_time) * 1) + app_count) * (variable * (time_block - sample_time))) + 1];			// Column
+	double *ar = new double[((((time_block - sample_time) * 1) + app_count) * (variable * (time_block - sample_time))) + 1];		// structural variable
+	/*============================== GLPK變數宣告(GLPK variable definition) =====================================*/
+	mip = glp_create_prob();
+	glp_set_prob_name(mip, "hardware_algorithm_case");
+	glp_set_obj_dir(mip, GLP_MIN);
+	glp_add_rows(mip, (((time_block - sample_time) * 1) + app_count));
+	glp_add_cols(mip, (variable * (time_block - sample_time)));	
+
+	/*=============================== 初始化矩陣(initial the matrix) ======================================*/
+	for (m = 0; m < (((time_block - sample_time) * 1) + app_count; m++)
+	{
+		for (n = 0; n < (variable * (time_block - sample_time)); n++)
+		{
+			power1[m][n] = 0.0;
+		}
+	}
+
+	for (h = 0; h < interrupt_num; h++)		// 可中斷負載(Interrupt load)
+	{
+		if ((interrupt_end[h] - sample_time) >= 0)
+		{
+			if ((interrupt_start[h] - sample_time) >= 0)
+			{
+				for (i = (interrupt_start[h] - sample_time); i <= (interrupt_end[h] - sample_time); i++)
+				{
+					power1[h][i*variable + h] = 1.0;
+				}
+			}
+			else if ((interrupt_start[h] - sample_time) < 0)
+			{
+				for (i = 0; i <= (interrupt_end[h] - sample_time); i++)
+				{
+					power1[h][i*variable + h] = 1.0;
+				}
+			}
+		}
+	}
+
+	// 決定是否輸出市電(Decide whether to buy electricity from utility)
+	for (i = 0; i < (time_block - sample_time); i++)
+	{
+		power1[app_count + i][i*variable + app_count] = -Pgrid_max; // Pgrid
+	}
+
+	/*============================== 宣告限制式條件範圍(row) ===============================*/
+	// GLPK讀列從1開始
+	// 限制式-家庭負載最低耗能
+	for (i = 1; i <= interrupt_num; i++)	// 可中斷負載(Interrupt load)
+	{
+		glp_set_row_name(mip, i, "");
+		glp_set_row_bnds(mip, i, GLP_LO, ((float)interrupt_reot[i - 1]), 0.0);	// ok
+	}
+
+	// 決定是否輸出市電
+	for (i = 1; i <= (time_block - sample_time); i++)
+	{
+		glp_set_row_name(mip, (app_count + i), "");
+		glp_set_row_bnds(mip, (app_count + i), GLP_UP, 0.0, 0.0);
+	}
+	
+	/*============================== 宣告決策變數(column) ================================*/
+	for (i = 0; i < (time_block - sample_time); i++)
+	{
+		for (j = 1; j <= app_count; j++)
+		{
+			glp_set_col_bnds(mip, (j + i*variable), GLP_DB, 0.0, 1.0);	// 負載決策變數
+			glp_set_col_kind(mip, (j + i*variable), GLP_BV);
+		}
+	}
+	
+	/*============================== GLPK寫入矩陣(ia,ja,ar) ===============================*/
+	for (i = 0; i < (((time_block - sample_time) * 1) + app_count); i++)
+	{
+		for (j = 0; j < (variable * (time_block - sample_time)); j++)
+		{
+			ia[i*((time_block - sample_time)*variable) + j + 1] = i + 1;
+			ja[i*((time_block - sample_time)*variable) + j + 1] = j + 1;
+			ar[i*((time_block - sample_time)*variable) + j + 1] = power1[i][j];
+		}
+	}
 }
 
