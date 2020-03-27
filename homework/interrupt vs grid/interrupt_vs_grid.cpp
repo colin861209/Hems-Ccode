9866dbb8a1dd5b05e48790058445877b03d6f129
colinwang from  HomeDesktop <colin861209@gmail.com>
2020-03-27 22:34:33 +0800

    update

--- homework/interrupt vs grid/interrupt_vs_grid.cpp
+++ homework/interrupt vs grid/interrupt_vs_grid.cpp
@@ -23,11 +23,14 @@ time_t t = time(NULL);
 struct tm now_time = *localtime(&t);
 
 char column[400] = "A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19,A20,A21,A22,A23,A24,A25,A26,A27,A28,A29,A30,A31,A32,A33,A34,A35,A36,A37,A38,A39,A40,A41,A42,A43,A44,A45,A46,A47,A48,A49,A50,A51,A52,A53,A54,A55,A56,A57,A58,A59,A60,A61,A62,A63,A64,A65,A66,A67,A68,A69,A70,A71,A72,A73,A74,A75,A76,A77,A78,A79,A80,A81,A82,A83,A84,A85,A86,A87,A88,A89,A90,A91,A92,A93,A94,A95";
+
+MYSQL *mysql_con = mysql_init(NULL);
+MYSQL_RES *mysql_result;
+MYSQL_ROW mysql_row;
+
 int main(void) {
     
-    MYSQL *mysql_con = mysql_init(NULL);
-	MYSQL_RES *mysql_result;
-	MYSQL_ROW mysql_row;
+    
     if ((mysql_real_connect(mysql_con, "140.124.42.70", "root", "fuzzy314", "wang", 6666, NULL, 0)) == NULL) {
 
 		printf("Failed to connect to Mysql!\n");
@@ -173,12 +176,12 @@ void GLPK(int *interrupt_start, int *interrupt_end, int *interrupt_ot, int *inte
 		}
 	}
  
-
+	float *s = new float[time_block];
 	/*============================ 總規劃功率矩陣(Total planning power matrix) ====================================*/
 	float **power1 = NEW2D((((time_block - sample_time) * 1) + app_count), (variable * (time_block - sample_time)), float);
 
 	/*============================ GLPK參數矩陣定義(GLPK parameter matrix definition) ==================================*/
-	// glp_prob *mip;
+	glp_prob *mip;
 	int *ia = new int[((((time_block - sample_time) * 1) + app_count) * (variable * (time_block - sample_time))) + 1]; 			// Row
 	int *ja = new int[((((time_block - sample_time) * 1) + app_count) * (variable * (time_block - sample_time))) + 1];			// Column
 	double *ar = new double[((((time_block - sample_time) * 1) + app_count) * (variable * (time_block - sample_time))) + 1];		// structural variable
